1c37dbfe9b9f79d37ff38ab85a786af6541a2876
p0txky <1000100ten@gmail.com>
2021-06-03 16:07:19 +0800

    remove message

--- config.h
+++ config.h
@@ -11,13 +11,3 @@ static const char *colorname[NUMCOLS] = {
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
-
-
-/* default message */
-static const char * message = "Please enter your password! ";
-
-/* text color */
-static const char * text_color = "#ffffff";
-
-/* text size (must be a valid size) */
-static const char * font_name = "9x15";
